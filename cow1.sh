cabc1e0dd199a58195d2bff0a356878df41375d4
Nivedh-Biju <nivedh0203@gmail.com>
2023-05-11 00:10:46 +0530

    adding cow1.sh

---
+++ cow1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+if [ -z  $1 ]; thenn         #checking if $1 (which is the first positional argument) is null
+   fortune | cowsay          #pipes the output o fortune into cowsay
+else 
+   cowsay $1                 #cowsays the exit code of the first positional argument which is its value stored in the $1
+fi

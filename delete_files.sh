13e59609e58a22a649f03655e764571d6b5a3933
Nivedh-Biju <nivedh0203@gmail.com>
2023-05-11 16:36:47 +0530

    new commit

--- delete_files.sh
+++ delete_files.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-for n in $(ls | grep .txt)  #pipes the output of ls into grep which checks if the file has .txt in its name . this is given as a range for the for loop to iterate
-do
-  rm -r $n                   #rm removes file and -r indicates recursive removal
-done
+find -name "*.txt" -type f -delete
 
